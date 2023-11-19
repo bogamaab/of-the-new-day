6b78ba41fc29392f01d3016496d4632491d5bcfb
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2023-11-19 16:58:58 -0500

    add visit controller and its views

---
+++ app/controllers/visits_controller.rb
@@ -0,0 +1,10 @@
+class VisitsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'visits/index'
+  get 'visits/show'
+  get 'visits/update'
   devise_for :users, path: 'users', controllers: { sessions: 'users/sessions' },
     path_name: { sign_in: 'login', sign_out: 'logout' }
 
